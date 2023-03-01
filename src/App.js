088f5c69eb0579f2cfc09f1416002ebc707fae43
Usman Muhammad <ozzmanmuhammad@gmail.com>
2023-03-01 21:19:36 +0500

    ...

---
+++ src/App.js
@@ -0,0 +1,108 @@
+// Import dependencies
+import React, { useRef, useState, useEffect } from "react";
+import * as tf from "@tensorflow/tfjs";
+import * as cocossd from "@tensorflow-models/coco-ssd";
+import Webcam from "react-webcam";
+import "./App.css";
+import { drawRect } from "./utilities";
+
+function App() {
+  const [Toggle, setToggle] = useState(false);
+
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  // Main function
+  const runCoco = async () => {
+    const net = await cocossd.load();
+    console.log("model loaded.");
+    //  Loop and detect hands
+    setInterval(() => {
+      detect(net);
+    }, 10);
+  };
+
+  const detect = async (net) => {
+    // Check data is available
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      // Get Video Properties
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      // Set video width
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      // Set canvas height and width
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+
+      // Make Detections
+      const obj = await net.detect(video);
+
+      // Draw mesh
+      const ctx = canvasRef.current.getContext("2d");
+      drawRect(obj, ctx); 
+    }
+  };
+
+  const toggle = () => {
+    setToggle(!Toggle)
+  };
+
+
+  useEffect(()=>{runCoco()},[]);
+
+  return (
+    <div className="App">
+      <h1 style={{margin:0,paddingTop: '2%', backgroundColor:"#282c34", color:"white"}}>OBJECT DETECTION</h1>
+      <button onClick={toggle} className="button">WEBCAM</button>
+      
+        <div className="App-header">
+        {Toggle && (
+          <>
+            <Webcam
+              ref={webcamRef}
+              muted={true} 
+              style={{
+                position: "absolute",
+                marginTop: "0",
+                marginLeft: "auto",
+                marginRight: "auto",
+                left: 0,
+                right: 0,
+                textAlign: "center",
+                zindex: 9,
+                width: 640,
+                height: 480,
+              }}
+            />
+
+            <canvas
+              ref={canvasRef}
+              style={{
+                position: "absolute",
+                marginLeft: "auto",
+                marginRight: "auto",
+                left: 0,
+                right: 0,
+                textAlign: "center",
+                zindex: 8,
+                width: 640,
+                height: 480,
+              }}
+            />
+          </>
+        )}
+        </div>
+      
+    </div>
+  );
+}
+
+export default App;
